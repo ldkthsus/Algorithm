fa0634a4d710ef141115af77f3191d5d971d59a8
YEONNI <66072766+ldkthsus@users.noreply.github.com>
2024-10-08 22:43:00 +0900

    [Silver V] Title: 나이순 정렬, Time: 1380 ms, Memory: 62748 KB -BaekjoonHub

---
+++ 백준/Silver/10814. 나이순 정렬/나이순 정렬.java
@@ -0,0 +1,46 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        int n = Integer.parseInt(br.readLine());
+        member [] members = new member[n];
+
+        for (int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine());
+            int age = Integer.parseInt(st.nextToken());
+            String name = st.nextToken();
+            members[i] = new member(age, name, i);
+        }
+
+        Arrays.sort(members);
+        for (int i = 0; i < members.length; i++) {
+            System.out.println(members[i].age + " " + members[i].name);
+        }
+    }
+
+    public static class member implements Comparable<member> {
+        int age;
+        String name;
+        int num;
+
+        public member(int age, String name, int num) {
+            this.age = age;
+            this.name = name;
+            this.num = num;
+        }
+
+        @Override
+        public int compareTo(member o) {
+            if(this.age == o.age){
+                return this.num - o.num;
+            }
+            return this.age - o.age;
+        }
+    }
+}
