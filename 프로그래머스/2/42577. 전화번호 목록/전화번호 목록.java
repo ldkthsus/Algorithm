d748920b8f03b8ef836eb255e860a9119af17226
YEONNI <66072766+ldkthsus@users.noreply.github.com>
2024-10-11 23:18:40 +0900

    [level 2] Title: 전화번호 목록, Time: 335.04 ms, Memory: 205 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
@@ -4,12 +4,17 @@ import java.io.*;
 class Solution {
     public boolean solution(String[] phone_book) {
         boolean answer = true;
-        Arrays.sort(phone_book);
+        Set<String> set = new HashSet<>();
         
-        for(int i=0;i<phone_book.length-1;i++){
-            if(phone_book[i+1].startsWith(phone_book[i])){
-                answer = false;
-                break;
+        for(int i=0;i<phone_book.length;i++){
+            set.add(phone_book[i]);
+        }
+        for(String phone : phone_book){
+            for(int i=0; i<phone.length();i++){
+                if(set.contains(phone.substring(0, i))){
+                    answer = false;
+                    break;
+                }
             }
         }
         return answer;
