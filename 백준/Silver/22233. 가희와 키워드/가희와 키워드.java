156a12e0536395e87adb7a3628ad10c8f0968b25
YEONNI <66072766+ldkthsus@users.noreply.github.com>
2025-08-17 16:38:55 +0900

    [Silver III] Title: 가희와 키워드, Time: 3272 ms, Memory: 305656 KB -BaekjoonHub

---
+++ 백준/Silver/22233. 가희와 키워드/가희와 키워드.java
@@ -0,0 +1,36 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+
+        Map<String, Integer> map = new HashMap<>();
+
+        for(int i = 0; i < N; i++){
+            String str = br.readLine();
+            map.put(str, 1);
+        }
+
+        for(int i = 0; i < M; i++){
+            String str = br.readLine();
+            String []arr = str.split(",");
+
+            for(int j = 0; j < arr.length; j++){
+                if(map.containsKey(arr[j])){
+                    map.remove(arr[j]);
+                }
+            }
+            System.out.println(map.size());
+        }
+    }
+}
