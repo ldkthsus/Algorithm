848e46276e4230af396709f8e27104f2ed5ce1cc
YEONNI <66072766+ldkthsus@users.noreply.github.com>
2025-10-17 22:59:26 +0900

    [level 3] Title: 조회수가 가장 많은 중고거래 게시판의 첨부파일 조회하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/3/164671. 조회수가 가장 많은 중고거래 게시판의 첨부파일 조회하기/조회수가 가장 많은 중고거래 게시판의 첨부파일 조회하기.sql
@@ -0,0 +1,8 @@
+select concat('/home/grep/src/', board_id, '/', file_id, file_name, file_ext) as file_path
+from used_goods_file
+where board_id in (
+select board_id
+    from used_goods_board
+    where views = (select max(views) from used_goods_board)
+)
+order by file_id desc;
