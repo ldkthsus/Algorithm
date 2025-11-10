07359746e5850047c9609e83cfefdb8931591850
YEONNI <66072766+ldkthsus@users.noreply.github.com>
2025-11-10 20:38:24 +0900

    [Silver IV] Title: 동방 프로젝트 (Small), Time: 68 ms, Memory: 11600 KB -BaekjoonHub

---
+++ 백준/Silver/14594. 동방 프로젝트 （Small）/동방 프로젝트 （Small）.java
@@ -0,0 +1,31 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        int n = Integer.parseInt(br.readLine());
+        int m = Integer.parseInt(br.readLine());
+        int cnt = 0;
+
+        boolean check[] = new boolean[n + 1];
+
+        for (int i = 0; i < m; i++) {
+            st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+
+            for (int j = a; j < b; j++) {
+                if(!check[j + 1]){
+                    cnt++;
+                    check[j + 1] = true;
+                }
+            }
+        }
+        System.out.println(n - cnt);
+    }
+}
