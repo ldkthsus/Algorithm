02ff08b94f5cd6e980dfc89d4e7369a687da7670
YEONNI <66072766+ldkthsus@users.noreply.github.com>
2025-12-15 17:28:35 +0900

    [Silver III] Title: queuestack, Time: 468 ms, Memory: 75724 KB -BaekjoonHub

---
+++ 백준/Silver/24511. queuestack/queuestack.java
@@ -0,0 +1,40 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        int n = Integer.parseInt(br.readLine());
+        int arr[] = new int[n];
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        int number[] = new int[n];
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            number[i] = Integer.parseInt(st.nextToken());
+        }
+
+        List<Integer> list = new ArrayList<>();
+        for (int i = n - 1; i >= 0; i--) {
+            if (arr[i] == 0) {
+                list.add(number[i]);
+            }
+        }
+
+        int m = Integer.parseInt(br.readLine());
+        st = new StringTokenizer(br.readLine());
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < m; i++) {
+            int num = Integer.parseInt(st.nextToken());
+            list.add(num);
+            sb.append(list.get(i)).append(" ");
+        }
+        System.out.println(sb);
+    }
+}
