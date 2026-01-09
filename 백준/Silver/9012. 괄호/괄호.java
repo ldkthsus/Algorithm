98200e0cf0bc6312e6476aa86873a813fbab3de6
YEONNI <66072766+ldkthsus@users.noreply.github.com>
2026-01-09 09:48:58 +0900

    [Silver IV] Title: 괄호, Time: 68 ms, Memory: 11672 KB -BaekjoonHub

--- 백준/Silver/9012. 괄호/괄호.java
+++ 백준/Silver/9012. 괄호/괄호.java
@@ -2,38 +2,34 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Stack;
+import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int t = Integer.parseInt(br.readLine());
+        int n = Integer.parseInt(br.readLine());
         StringBuilder sb = new StringBuilder();
 
-        for (int tc = 0; tc < t; tc++) {
+        for (int i = 0; i < n; i++) {
             String str = br.readLine();
             Stack<Character> stack = new Stack<>();
-            for (int i = 0; i < str.length(); i++) {
-                char now = str.charAt(i);
+            boolean check = true;
 
-                if (stack.isEmpty()) {
-                    stack.push(now);
-                } else {
-                    if (now == '(') {
-                        stack.push(now);
-                    } else {
-                        if (stack.peek() == '(') {
-                            stack.pop();
-                        }
-                    }
-                }
+            for (int j = 0; j < str.length(); j++) {
+                char now = str.charAt(j);
+                if (now == ')') {
+                    if (stack.isEmpty()) {
+                        sb.append("NO").append("\n");
+                        check = false;
+                        break;
+                    } else stack.pop();
+                } else stack.push(')');
             }
-
-            if (stack.isEmpty()) {
-                sb.append("YES");
-            } else {
-                sb.append("NO");
+            if (check) {
+                if (!stack.isEmpty()) {
+                    sb.append("NO").append("\n");
+                } else sb.append("YES").append("\n");
             }
-            sb.append("\n");
         }
         System.out.println(sb);
     }
