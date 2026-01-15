80a9483af16dc7a1d636380b78ad0998ac723778
YEONNI <66072766+ldkthsus@users.noreply.github.com>
2026-01-15 14:34:11 +0900

    [Silver IV] Title: 나는야 포켓몬 마스터 이다솜, Time: 416 ms, Memory: 53892 KB -BaekjoonHub

--- 백준/Silver/1620. 나는야 포켓몬 마스터 이다솜/나는야 포켓몬 마스터 이다솜.java
+++ 백준/Silver/1620. 나는야 포켓몬 마스터 이다솜/나는야 포켓몬 마스터 이다솜.java
@@ -9,25 +9,23 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-
         int n = Integer.parseInt(st.nextToken());
         int m = Integer.parseInt(st.nextToken());
 
+        String arr[] = new String[n + 1];
         Map<String, Integer> map = new HashMap<>();
-        Map<Integer, String> dogam = new HashMap<>();
-        for (int i = 1; i <= n; i++) {
-            String str = br.readLine();
-            map.put(str, i);
-            dogam.put(i, str);
-        }
 
+        for (int i = 1; i <= n; i++) {
+            String name = br.readLine();
+            arr[i] = name;
+            map.put(name, i);
+        }
         StringBuilder sb = new StringBuilder();
-        for(int i = 0; i < m; i++){
+        for (int i = 0; i < m; i++) {
             String str = br.readLine();
-            if(Character.isDigit(str.charAt(0))){
-                sb.append(dogam.get(Integer.parseInt(str))).append("\n");
-            }
-            else{
+            if (Character.isDigit(str.charAt(0))) {
+                sb.append(arr[Integer.parseInt(str)]).append("\n");
+            } else {
                 sb.append(map.get(str)).append("\n");
             }
         }
