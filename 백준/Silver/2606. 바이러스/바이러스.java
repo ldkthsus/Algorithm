d0a18317f47357207a1e6f5fc7cbdffb58b5a927
YEONNI <66072766+ldkthsus@users.noreply.github.com>
2026-01-17 20:42:34 +0900

    [Silver III] Title: 바이러스, Time: 68 ms, Memory: 11736 KB -BaekjoonHub

---
+++ 백준/Silver/2606. 바이러스/바이러스.java
@@ -0,0 +1,47 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayDeque;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static int n, m, arr[][], cnt;
+    public static boolean check[];
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        n = Integer.parseInt(br.readLine()); //컴퓨터 개수
+        m = Integer.parseInt(br.readLine());
+        arr = new int[n + 1][n + 1];
+        check = new boolean[n + 1];
+
+        for (int i = 0; i < m; i++) {
+            st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+
+            arr[a][b] = 1;
+            arr[b][a] = 1;
+        }
+        bfs(1);
+        System.out.println(cnt);
+    }
+
+    public static void bfs(int v) {
+        Queue<Integer> queue = new ArrayDeque<>();
+        queue.offer(v);
+        check[v] = true;
+        while (!queue.isEmpty()) {
+            int now = queue.poll();
+            for (int i = 1; i <= n; i++) {
+                if (arr[now][i] == 1 && !check[i]) {
+                    queue.offer(i);
+                    check[i] = true;
+                    cnt++;
+                }
+            }
+        }
+    }
+}
