cc3409cf8234687782290fd8eceba84d45040d69
YEONNI <66072766+ldkthsus@users.noreply.github.com>
2026-01-20 09:21:02 +0900

    [Bronze V] Title: 문자열, Time: 60 ms, Memory: 11332 KB -BaekjoonHub

---
+++ 백준/Bronze/9086. 문자열/문자열.java
@@ -0,0 +1,16 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < n; i++) {
+            String str = br.readLine();
+            sb.append(str.charAt(0)).append(str.charAt(str.length() - 1)).append("\n");
+        }
+        System.out.println(sb);
+    }
+}
