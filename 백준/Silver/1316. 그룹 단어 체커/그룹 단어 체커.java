063cd861fe3fea80e96b1caeb74550fe604e737f
YEONNI <66072766+ldkthsus@users.noreply.github.com>
2026-01-21 16:19:48 +0900

    [Silver V] Title: 그룹 단어 체커, Time: 60 ms, Memory: 11580 KB -BaekjoonHub

--- 백준/Silver/1316. 그룹 단어 체커/그룹 단어 체커.java
+++ 백준/Silver/1316. 그룹 단어 체커/그룹 단어 체커.java
@@ -1,34 +1,33 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
+        boolean alpha[] = new boolean[26];
         int n = Integer.parseInt(br.readLine());
-        int cnt = 0;
-
-        for (int t = 0; t < n; t++) {
+        int ans = 0;
+        for(int i = 0; i < n; i++){
             String str = br.readLine();
-            boolean alpha[] = new boolean[26];
+            Arrays.fill(alpha, false);
             boolean check = true;
-
-            for(int i = 0; i < str.length(); i++){
-                char word = str.charAt(i);
-                if(!alpha[word - 'a']){
-                    alpha[word - 'a'] = true;
-                }
-                else{
-                    if(word != str.charAt(i - 1)){
+            for(int j = 0; j < str.length(); j++){
+                char now = str.charAt(j);
+                if(!alpha[now - 'a']){
+                    alpha[now - 'a'] = true;
+                }else{
+                    char pre = str.charAt(j - 1);
+                    if(pre != now){
                         check = false;
                         break;
                     }
                 }
             }
             if(check)
-                cnt++;
+                ans++;
         }
-        System.out.println(cnt);
+        System.out.println(ans);
     }
 }
