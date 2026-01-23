eb47f62714e1eebd6b8511fa29da061d12e9e6b1
YEONNI <66072766+ldkthsus@users.noreply.github.com>
2026-01-23 15:48:42 +0900

    [Silver III] Title: N과 M (4), Time: 92 ms, Memory: 14300 KB -BaekjoonHub

--- 백준/Silver/15652. N과 M （4）/N과 M （4）.java
+++ 백준/Silver/15652. N과 M （4）/N과 M （4）.java
@@ -1,40 +1,43 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
-	static int N,M, result[], number[];
-	static StringBuilder sb;
-	
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		sb = new StringBuilder();
-		
-		N = sc.nextInt();
-		M = sc.nextInt();
-		
-		number = new int[N];
-		result = new int [M];
-		
-		for(int i=0;i<N;i++) {
-			number[i] = i+1;
-		}
-		
-		중복조합(0, 0);
-		System.out.println(sb.toString());
-	}
-	
-	public static void 중복조합(int idx, int start) {
-		if(idx==M) {
-			for(int i=0;i<M;i++) {
-				sb.append(result[i] + " ");
-			}
-			sb.append('\n');
-			return;
-		}
-		
-		for(int i=start;i<N;i++) {
-			result[idx] = number[i];
-			중복조합(idx+1, i);
-		}
-	}
+    public static int n, m, result[];
+    public static boolean isUsed[];
+    public static StringBuilder sb;
 
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+        isUsed = new boolean[n];
+        result = new int[m];
+        sb = new StringBuilder();
+        perm(0);
+        System.out.println(sb);
+    }
+
+    public static void perm(int idx) {
+        if (idx == m) {
+            for (int j = 0; j < m; j++) {
+                sb.append(result[j]).append(" ");
+            }
+            sb.append("\n");
+            return;
+        }
+        for (int i = 1; i <= n; i++) {
+            if (idx == 0) {
+                result[idx] = i;
+                perm(idx + 1);
+            } else {
+                if (result[idx - 1] <= i) {
+                    result[idx] = i;
+                    perm(idx + 1);
+                }
+            }
+        }
+    }
 }
