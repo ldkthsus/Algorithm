bb91cf759ef682fbc97f7dcc2a9765aa8925e6db
YEONNI <66072766+ldkthsus@users.noreply.github.com>
2026-02-12 15:45:52 +0900

    [Silver III] Title: 고양이 카페, Time: 96 ms, Memory: 13732 KB -BaekjoonHub

---
+++ 백준/Silver/28353. 고양이 카페/고양이 카페.java
@@ -0,0 +1,39 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int k = Integer.parseInt(st.nextToken());
+
+        int arr[] = new int[n];
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        int start = 0;
+        int end = n - 1;
+        int cnt = 0;
+        Arrays.sort(arr);
+        while(true){
+            if(start >= end){
+                break;
+            }
+            if(arr[start] + arr[end] <= k){
+                cnt++;
+                start++;
+                end--;
+            }else{
+                end--;
+            }
+        }
+        System.out.println(cnt);
+    }
+}
