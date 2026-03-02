fec775211c1a388a0e446a6647fc210bf2cb7640
YEONNI <66072766+ldkthsus@users.noreply.github.com>
2026-03-02 09:42:59 +0900

    [Silver III] Title: N과 M (7), Time: 564 ms, Memory: 220008 KB -BaekjoonHub

---
+++ 백준/Silver/15656. N과 M （7）/N과 M （7）.java
@@ -0,0 +1,42 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static int n, m, result[], number[];
+    public static StringBuilder sb;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+        result = new int[m];
+        number = new int[n];
+        sb = new StringBuilder();
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            number[i] = Integer.parseInt(st.nextToken());
+        }
+        Arrays.sort(number);
+        perm(0);
+        System.out.println(sb);
+    }
+
+    public static void perm(int idx) {
+        if (idx == m) {
+            for (int i = 0; i < m; i++) {
+                sb.append(result[i]).append(" ");
+            }
+            sb.append("\n");
+            return;
+        }
+        for (int i = 0; i < n; i++) {
+            result[idx] = number[i];
+            perm(idx + 1);
+        }
+    }
+}
