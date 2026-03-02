ea5683b8f199047ed1d85d6dcf435f4412689dc0
YEONNI <66072766+ldkthsus@users.noreply.github.com>
2026-03-02 09:19:27 +0900

    [Silver III] Title: N과 M (5), Time: 200 ms, Memory: 30644 KB -BaekjoonHub

--- 백준/Silver/15654. N과 M （5）/N과 M （5）.java
+++ 백준/Silver/15654. N과 M （5）/N과 M （5）.java
@@ -1,53 +1,48 @@
-    import java.io.BufferedReader;
-    import java.io.IOException;
-    import java.io.InputStreamReader;
-    import java.util.Arrays;
-    import java.util.StringTokenizer;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
 
-    public class Main {
-        public static int num[], result[], n, m;
-        public static boolean used[];
-        public static StringBuilder sb;
+public class Main {
+    public static int number[], result[], n, m;
+    public static boolean isSelected[];
+    public static StringBuilder sb;
 
-        public static void main(String[] args) throws IOException {
-            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-            StringTokenizer st;
-
-            st = new StringTokenizer(br.readLine());
-
-            n = Integer.parseInt(st.nextToken());
-            m = Integer.parseInt(st.nextToken());
-
-            num = new int[n];
-            result = new int[m];
-            used = new boolean[n];
-
-            st = new StringTokenizer(br.readLine());
-            for (int i = 0; i < n; i++) {
-                num[i] = Integer.parseInt(st.nextToken());
-            }
-            Arrays.sort(num);
-            sb = new StringBuilder();
-            perm(0);
-            System.out.println(sb);
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+        number = new int[n];
+        result = new int[m];
+        isSelected = new boolean[n];
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            number[i] = Integer.parseInt(st.nextToken());
         }
+        sb = new StringBuilder();
+        Arrays.sort(number);
+        perm(0);
+        System.out.println(sb);
+    }
 
-        public static void perm(int idx) {
-            if (idx == m) {
-                for (int i = 0; i < m; i++) {
-                    sb.append(result[i]).append(" ");
-                }
-                sb.append("\n");
-                return;
+    public static void perm(int idx) {
+        if (idx == m) {
+            for (int i = 0; i < m; i++) {
+                sb.append(result[i]).append(" ");
             }
-
-            for (int i = 0; i < n; i++) {
-                if(!used[i]){
-                    result[idx] = num[i];
-                    used[i] = true;
-                    perm(idx + 1);
-                    used[i] = false;
-                }
+            sb.append("\n");
+            return;
+        }
+        for (int i = 0; i < n; i++) {
+            if (!isSelected[i]) {
+                result[idx] = number[i];
+                isSelected[i] = true;
+                perm(idx + 1);
+                isSelected[i] = false;
             }
         }
     }
+}
