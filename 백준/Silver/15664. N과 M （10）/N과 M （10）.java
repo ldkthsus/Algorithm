18316c3d31433c6eba258032127d6bf83747fc19
YEONNI <66072766+ldkthsus@users.noreply.github.com>
2026-03-04 09:14:51 +0900

    [Silver II] Title: N과 M (10), Time: 64 ms, Memory: 11636 KB -BaekjoonHub

---
+++ 백준/Silver/15664. N과 M （10）/N과 M （10）.java
@@ -0,0 +1,51 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static int n, m, number[], result[];
+    public static StringBuilder sb;
+    public static Map<String, Integer> map;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+
+        number = new int[n];
+        result = new int[m];
+        sb = new StringBuilder();
+        map = new HashMap<>();
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            number[i] = Integer.parseInt(st.nextToken());
+        }
+        Arrays.sort(number);
+        comb(0, 0);
+        System.out.println(sb);
+    }
+
+    public static void comb(int idx, int start) {
+        if (idx == m) {
+            StringBuilder temp = new StringBuilder();
+            for (int i = 0; i < m; i++) {
+                temp.append(result[i]).append(" ");
+            }
+            if (!map.containsKey(temp.toString())) {
+                map.put(temp.toString(), 1);
+                sb.append(temp).append("\n");
+            }
+            return;
+        }
+        for (int i = start; i < n; i++) {
+            result[idx] = number[i];
+            comb(idx + 1, i + 1);
+        }
+    }
+}
