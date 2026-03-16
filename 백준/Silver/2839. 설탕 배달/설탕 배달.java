41f492a79ca7363e39ab4a0cd8694b800e2fd97e
YEONNI <66072766+ldkthsus@users.noreply.github.com>
2026-03-16 09:45:15 +0900

    [Silver IV] Title: 설탕 배달, Time: 68 ms, Memory: 11512 KB -BaekjoonHub

---
+++ 백준/Silver/2839. 설탕 배달/설탕 배달.java
@@ -0,0 +1,35 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int five = n / 5;
+        int rest = n - (5 * five);
+        int three = rest / 3;
+        boolean check = true;
+        if (five == 0 && rest % 3 != 0) {
+            System.out.println(-1);
+            return;
+        }
+
+        while (true) {
+            if (five < 0) {
+                check = false;
+                break;
+            }
+            if (rest % 3 == 0) {
+                break;
+            }
+            five--;
+            rest = n - (5 * five);
+            three = rest / 3;
+        }
+        if (!check)
+            System.out.println(-1);
+        else
+            System.out.println(five + three);
+    }
+}
