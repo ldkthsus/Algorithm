9ab9a48d8ec67d002835e1855b8b3fe47231f9ba
YEONNI <66072766+ldkthsus@users.noreply.github.com>
2026-04-15 20:52:54 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 58.35 ms, Memory: 112 MB -BaekjoonHub

---
+++ 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+public class Solution {
+    public int[] solution(int []arr) {
+        Stack<Integer> stack = new Stack<>();
+        for(int i = 0; i < arr.length; i++){
+            if(stack.isEmpty()){
+                stack.push(arr[i]);
+            }else{
+                if(stack.peek() != arr[i]){
+                    stack.push(arr[i]);
+                }
+            }
+        }
+        int size = stack.size();
+        int[] answer = new int[size];
+        for(int i = size - 1; i >=0; i--){
+            answer[i] = stack.pop();
+        }
+        return answer;
+    }
+}
