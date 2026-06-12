75b71be626e2de5cb1cfbf03780fb499668645f2
YEONNI <66072766+ldkthsus@users.noreply.github.com>
2026-06-12 10:25:48 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 75.49 ms, Memory: 126 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -1,29 +1,35 @@
-import java.util.*;
 import java.io.*;
+import java.util.*;
 
 class Solution {
     public String solution(String[] participant, String[] completion) {
-        String answer = "";
-        Map<String, Integer> map = new HashMap<String, Integer>();
-        for(int i=0;i<participant.length;i++){
-            if(map.containsKey(participant[i])){
-                map.put(participant[i], map.get(participant[i]) + 1);
-            }
-            else{
-            map.put(participant[i], 1);}
+        Map<String, Integer> par = new HashMap<>();
+        Map<String, Integer> com = new HashMap<>();
+        for(int i = 0; i < participant.length; i++){
+            if(!par.containsKey(participant[i])){
+                par.put(participant[i], 1);
+            } else par.put(participant[i], par.get(participant[i]) + 1);
         }
-        
-        for(int i=0;i<completion.length;i++){
-            if(map.containsKey(completion[i])){
-                map.put(completion[i], map.get(completion[i])-1);
-            }
+        for(int i = 0; i < completion.length; i++){
+            if(!com.containsKey(completion[i])){
+                com.put(completion[i], 1);
+            } else com.put(completion[i], com.get(completion[i]) + 1);
         }
-        for(String key : map.keySet()) {
-            if(map.get(key) > 0){
+         String answer = "";
+        for(String key : par.keySet()){
+            if(com.containsKey(key)){
+                int val = com.get(key);
+                int start = par.get(key);
+                
+                if(val != start){
+                    answer = key;
+                    break;
+                }
+            } else{
                 answer = key;
+                break;
             }
         }
-        
-           return answer;
-        }
+        return answer;
     }
+}
