16685250be976982b7768f8db29382d1721bf09e
YEONNI <66072766+ldkthsus@users.noreply.github.com>
2026-07-10 16:48:48 +0900

    [level 1] Title: 수박수박수박수박수박수?, Time: 27.41 ms, Memory: 132 MB -BaekjoonHub

---
+++ 프로그래머스/1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.java
@@ -0,0 +1,14 @@
+import java.io.*;
+import java.util.*;
+
+class Solution {
+    public String solution(int n) {
+        String answer = "";
+        for(int i = 0; i < n; i++){
+            if(i % 2 == 0)
+                answer += "수";
+            else answer += "박";
+        }
+        return answer;
+    }
+}
