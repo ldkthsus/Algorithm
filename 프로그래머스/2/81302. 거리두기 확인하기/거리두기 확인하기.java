4669aeb405a2b85339a3f1b00a901c5e74a7e9b2
YEONNI <66072766+ldkthsus@users.noreply.github.com>
2026-09-24 23:19:43 +0900

    [level 2] Title: 거리두기 확인하기, Time: 0.04 ms, Memory: 71.7 MB -BaekjoonHub

--- 프로그래머스/2/81302. 거리두기 확인하기/거리두기 확인하기.java
+++ 프로그래머스/2/81302. 거리두기 확인하기/거리두기 확인하기.java
@@ -62,12 +62,4 @@ class Solution {
         }
         return answer;
     }
-    public static class Point{
-        int i;
-        int j;
-        public Point(int i, int j){
-            this.i = i;
-            this.j = j;
-        }
-    }
 }
